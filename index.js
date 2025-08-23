1176f8c455f6cf6f99b323f27cf22c32a14a05f1
Super Mesho <174741494+SuperMesho7@users.noreply.github.com>
2025-08-23 04:19:54 +0300

    Quick Commit

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 function login() {
-    window.location.href = "http://localhost:12784/auth/login"
+  window.location.href = "http://localhost:12784/auth/login";
 }
 
 fetch("http://localhost:12784/me/guilds", {
-  credentials: "include"
+  credentials: "include",
 })
-.then(res => res.json())
-.then(data => {
- for (let index = 0; index < data.length; index++) {
-  const guild = data[index];
-  guildname = guild["name"] + " "
-    document.getElementById("me").innerText += guildname
- }
-    
-});
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((guild) => {
+      guildname = guild["name"] + " ";
+      document.getElementById("me").innerText += guildname;
+    });
+  });
