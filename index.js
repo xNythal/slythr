13282e435ba040c28034471e166df2560d340069
Super Mesho <174741494+SuperMesho7@users.noreply.github.com>
2025-08-23 04:11:46 +0300

    Quick Commit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ function login() {
     window.location.href = "http://localhost:12784/auth/login"
 }
 
-fetch("http://localhost:12784/me", {
+fetch("http://localhost:12784/me/guilds", {
   credentials: "include"
 })
 .then(res => res.json())
 .then(data => {
-  data["guilds"].forEach(guild => {
+  data.forEach(guild => {
     guildname = guild["name"] + " "
     document.getElementById("me").innerText += guildname
   });
