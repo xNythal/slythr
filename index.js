aedef6008798c77be4913a5da23d4b153adb5562
Super Mesho <174741494+SuperMesho7@users.noreply.github.com>
2025-08-23 05:15:05 +0300

    Quick Commit

--- index.js
+++ index.js
@@ -5,14 +5,3 @@ function login() {
 function dashboard() {
   window.location.href = "dashboard.html"
 }
-
-fetch("http://localhost:12784/me/guilds?owner=true", {
-  credentials: "include",
-})
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((guild) => {
-      guildname = guild["name"] + " ";
-      document.getElementById("me").innerText += guildname;
-    });
-  });
